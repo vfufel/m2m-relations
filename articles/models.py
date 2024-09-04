a54c5e38f7564563c06d9e9c409a49344213bf67
vfufel <bubelev.vlad@gmail.com>
2024-09-04 14:23:32 +0300

    Finished task

--- articles/models.py
+++ articles/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
+class Tag(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Название тега')
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.name
 
 class Article(models.Model):
 
@@ -7,10 +16,26 @@ class Article(models.Model):
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    tags = models.ManyToManyField(Tag, through='Scope', related_name='articles')
 
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
 
     def __str__(self):
         return self.title
+
+
+class Scope(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes')
+    is_main = models.BooleanField(default=False, verbose_name='Основной')
+
+    class Meta:
+        verbose_name = 'Связь статьи с тегом'
+        verbose_name_plural = 'Связи статей с тегами'
+        unique_together = ('article', 'tag')
+
+    def __str__(self):
+        return f"{self.article.title} - {self.tag.name}"
